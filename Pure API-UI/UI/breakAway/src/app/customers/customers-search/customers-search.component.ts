94006c23c79854e0a62b044de59eea8529231fc5
Valesca Sarrocchi <valesca.sarrocchi@stockholm.educations.com>
2019-10-04 15:35:19 +0200

    change input/ngModel in customers-search

--- Pure API-UI/UI/breakAway/src/app/customers/customers-search/customers-search.component.ts
+++ Pure API-UI/UI/breakAway/src/app/customers/customers-search/customers-search.component.ts
@@ -1,25 +1,25 @@
 import { Component, OnInit, Output, EventEmitter } from '@angular/core';
 
-import { CustomerService } from '../customer.service';
-
 @Component({
   selector: 'app-customers-search',
   templateUrl: './customers-search.component.html',
   styleUrls: ['./customers-search.component.css']
 })
 export class CustomersSearchComponent implements OnInit {
+  public variable: string;
 
   @Output() searchTerms = new EventEmitter<string>();
 
-  constructor(private customerService: CustomerService) { }
+  constructor() { }
 
-  search(term: string) {
-    this.searchTerms.emit(term);
-  }
-
-  // public variableChange(term: string): void {
+  // search(term: string) {
   //   this.searchTerms.emit(term);
   // }
+
+  public variableChange(term: string) {
+    this.variable = term;
+    this.searchTerms.emit(this.variable);
+  }
 
   ngOnInit(): void {
 
