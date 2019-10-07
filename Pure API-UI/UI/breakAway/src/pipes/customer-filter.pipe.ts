cb70a7549fcb16e8f20f384f4d8260fa53c8d6a7
Valesca Sarrocchi <valesca.sarrocchi@stockholm.educations.com>
2019-10-07 08:13:01 +0200

    test

--- Pure API-UI/UI/breakAway/src/pipes/customer-filter.pipe.ts
+++ Pure API-UI/UI/breakAway/src/pipes/customer-filter.pipe.ts
@@ -1,33 +1,35 @@
 import { Pipe, PipeTransform } from "@angular/core";
-import { Customer } from 'src/models/customer';
+import { Customer } from "src/models/customer";
 
-@Pipe({ name: 'customerFilter' })
+@Pipe({ name: "customerFilter" })
 export class CustomerFilterPipe implements PipeTransform {
-
-    // transform(customers: Array<Customer>, filter: string) {
-    transform(customers: Array<Customer>, prop: keyof Customer, value: string | number | boolean) {
-        if (!value || !customers) {
-            return customers;
-        }
-
-        const filter = this.getFilter(value);
-        return customers.filter(filter);
-
-        // return customers.filter(f => f.firstName.indexOf(filter.charAt(0).toUpperCase() + filter.slice(1)) !== -1 || f.lastName.indexOf(filter.charAt(0).toUpperCase() + filter.slice(1)) !== -1);
+  // transform(customers: Array<Customer>, filter: string) {
+  transform(
+    customers: Array<Customer>,
+    prop: keyof Customer,
+    value: string | number | boolean
+  ) {
+    if (!value || !customers) {
+      return customers;
     }
 
-    getFilter(value: string | number | boolean): (value: any) => Array<any> {
-        if (typeof(value) === "string") {
-            return this.stringFilter;
-        } else if (typeof(value) === "number") {
-            return this.numberFilter;
-        } else {
-            return this.booleanFilter;
-        }
-    } 
+    const filter = this.getFilter(value);
+    return customers.filter(filter);
 
-    stringFilter(item: any, prop: string, value: string): boolean {
-        item[prop].toLowerCase() === value.toLowerCase();
+    // return customers.filter(f => f.firstName.indexOf(filter.charAt(0).toUpperCase() + filter.slice(1)) !== -1 || f.lastName.indexOf(filter.charAt(0).toUpperCase() + filter.slice(1)) !== -1);
+  }
+
+  getFilter(value: string | number | boolean): (value: any) => Array<any> {
+    if (typeof value === "string") {
+      return this.stringFilter;
+    } else if (typeof value === "number") {
+      return this.numberFilter;
+    } else {
+      return this.booleanFilter;
     }
+  }
 
+  stringFilter(item: any, prop: string, value: string): boolean {
+    item[prop].toLowerCase() === value.toLowerCase();
+  }
 }
