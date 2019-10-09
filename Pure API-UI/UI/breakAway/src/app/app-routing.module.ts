5976f4af9ee37b581d5e5b7a8cd457b1056c15bc
Valesca Sarrocchi <valesca.sarrocchi@stockholm.educations.com>
2019-10-09 13:34:17 +0200

    add create customer link

--- Pure API-UI/UI/breakAway/src/app/app-routing.module.ts
+++ Pure API-UI/UI/breakAway/src/app/app-routing.module.ts
@@ -3,7 +3,6 @@ import { Routes, RouterModule } from '@angular/router';
 
 
 const routes: Routes = [
-  // { path: "/", redirectTo: "home/home-main", pathMatch: "full" },
   { path: "home/home-main", redirectTo: "/home", pathMatch: "full" },
   { path: "customers/customers-list", redirectTo: "/customer", pathMatch: "full" },
   { path: "activities/activities-list", redirectTo: "/activity", pathMatch: "full" }
